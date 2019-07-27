c393f4f956635f29bf06e6dcb53ecfb022eced79
ojaoc <c0rreia@hotmail.com>
2019-07-27 16:56:34 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let grid_div = document.getElementById('grid');
+
+let grid_container = document.createElement('div');
+let grid_row = document.createElement('div');
+let grid_column = document.createElement('div');
+
+grid_container.classList.add('grid_container');
+grid_row.classList.add('grid_row');
+grid_column.classList.add('grid_column');
+
+
+
+
+
