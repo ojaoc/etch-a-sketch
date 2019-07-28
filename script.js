0dc2f99775fce30b528f2b46200897cba0a9b208
ojaoc <c0rreia@hotmail.com>
2019-07-28 10:05:56 +0100

    grid creation DOM

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 let grid_div = document.getElementById('grid');
 
 let grid_container = document.createElement('div');
-let grid_row = document.createElement('div');
-let grid_column = document.createElement('div');
+
 
 grid_container.classList.add('grid_container');
-grid_row.classList.add('grid_row');
-grid_column.classList.add('grid_column');
+
+
+grid_div.appendChild(grid_container);
+grid_container.appendChild(grid_item);
+
+function createGrid(widthAndHeight) {
+    let numberOfCells = widthAndHeight ** 2;
+    for (let i = 0; i < numberOfCells; i++) {
+        let grid_item = document.createElement('div');
+        grid_item.classList.add('grid_item');
+        grid_container.appendChild(grid_item);
+    }
+    grid_container.setAttribute('style', `grid-template: repeat(${widthAndHeight}, 1fr) / repeat(${widthAndHeight}, 1fr);`);
+}
+
+
 
 
 
