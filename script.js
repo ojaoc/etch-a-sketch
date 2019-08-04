fbf7e19b7d04a76fd643a4cb2005ccb04f0d67e6
ojaoc <c0rreia@hotmail.com>
2019-08-04 20:35:47 +0100

    functional etch-a-sketch

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 // Select container
 let container_div = document.getElementById('grid');
-// Style container
-container_div.classList.add('grid_container'); 
 // Prompt size
 let size_button = document.getElementById('size');
-// Prompt answer
-size_button.addEventListener('click', function () {
-        let size = prompt('Set the size of the Etch-a-Sketch! It is a square, so the height and width are the same.', 16);
-        let widthAndHeight = size;
-        
-        function createGrid(widthAndHeight) {
-            let numberOfCells = widthAndHeight * widthAndHeight;
-            for (let i = 0; i < numberOfCells; i++) {
-                let grid_item = document.createElement('div');
-                grid_item.setAttribute('class', 'grid_item');
-                container_div.appendChild(grid_item);
-            }
-        
-            container_div.setAttribute('style', `grid-template-rows: repeat(${widthAndHeight}, 1fr); grid-template-columns: repeat(${widthAndHeight}, 1fr);`);
-        }
-});
 
+function createGrid(widthAndHeight) {
+    let numberOfCells = widthAndHeight * widthAndHeight;
+    for (let i = 0; i < numberOfCells; i++) {
+        let grid_item = document.createElement('div');
+        grid_item.setAttribute('class', 'grid_item');
+        container_div.appendChild(grid_item);
+    }
+
+    container_div.setAttribute('style', `grid-template-rows: repeat(${widthAndHeight}, 1fr); grid-template-columns: repeat(${widthAndHeight}, 1fr);`);
+}
+
+function selectColor(color) {
+    if (!color) {
+        color = 'black';
+    }
+    let gridBlockNodeList = container_div.querySelectorAll('div');
+    let gridBlocksArray = Array.from(gridBlockNodeList);
+    gridBlocksArray.forEach(block => {
+        block.addEventListener('mouseover', (e) => e.target.style.backgroundColor = color);
+    });
+}
+
+createGrid(16);
+selectColor();
 
 
 
