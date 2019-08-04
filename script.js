498ea60f33512991cae57bcef3a6170ecf1b7509
ojaoc <c0rreia@hotmail.com>
2019-08-04 05:45:10 +0100

    js logic

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-let grid_div = document.getElementById('grid');
+// Select container
+let container_div = document.getElementById('grid');
+// Style container
+container_div.classList.add('grid_container'); 
+// Prompt size
+let size_button = document.getElementById('size');
+// Prompt answer
+size_button.addEventListener('click', function () {
+        let size = prompt('Set the size of the Etch-a-Sketch! It is a square, so the height and width are the same.', 16);
+        let widthAndHeight = size;
+        
+        function createGrid(widthAndHeight) {
+            let numberOfCells = widthAndHeight * widthAndHeight;
+            for (let i = 0; i < numberOfCells; i++) {
+                let grid_item = document.createElement('div');
+                grid_item.setAttribute('class', 'grid_item');
+                container_div.appendChild(grid_item);
+            }
+        
+            container_div.setAttribute('style', `grid-template-rows: repeat(${widthAndHeight}, 1fr); grid-template-columns: repeat(${widthAndHeight}, 1fr);`);
+        }
+});
 
-let grid_container = document.createElement('div');
 
 
-grid_container.classList.add('grid_container');
 
 
-grid_div.appendChild(grid_container);
-grid_container.appendChild(grid_item);
-
-function createGrid(widthAndHeight) {
-    let numberOfCells = widthAndHeight ** 2;
-    for (let i = 0; i < numberOfCells; i++) {
-        let grid_item = document.createElement('div');
-        grid_item.classList.add('grid_item');
-        grid_container.appendChild(grid_item);
-    }
-    grid_container.setAttribute('style', `grid-template: repeat(${widthAndHeight}, 1fr) / repeat(${widthAndHeight}, 1fr);`);
-}
 
 
 
